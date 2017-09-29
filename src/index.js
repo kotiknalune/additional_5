a84456ed3793108348afa0cd25d8264c746f4213
Ekaterina Raylyan <31958491+kotiknalune@users.noreply.github.com>
2017-09-29 20:47:55 +0300

    version 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   var parentheses = "[]{}()",
+    bracketsConfig = [], i, object, bracketsLocation;
+  for(i = 0; object = str[i]; i++) {
+    bracketsLocation = parentheses.indexOf(object);
+    if(bracketsLocation === -1) {
+      continue;
+    }
+    if(bracketsLocation % 2 === 0) {
+      bracketsConfig.push(bracketsLocation + 1);
+    } else {
+      if(bracketsConfig.pop() !== bracketsLocation) {
+        return false;
+      }
+    }
+  }
+  return bracketsConfig.length === 0;
 }
