7a74ba88522de91d39a7ec4ffe322fc2c945ddf4
Katerina <katerina.raylyan@gmail.com>
2020-03-12 14:41:56 +0300

    fix: refactor the soluton

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function check(string, bracketsConfig) {
-  let brackets = []; // we create a new array
+  let brackets = []; str = string; // we create a new array
 
   for (let i = 0; i < bracketsConfig.length; i++) { // for every item in bC
     brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]); //
   }
   for (let i = 0; i < string.length; i++) {  // for every item in string
     for (let j = 0; j < brackets.length; j++) { // and evert item in new array
-      string = string.replace(brackets[j], ""); // we cross out the found bracket
+      str = str.replace(brackets[j], ""); // we cross out the found bracket
     }
-    if (string.length == 0) { // if nothing was added, the string matches
+    if (str.length == 0) { // if nothing was added, the string matches
       return true;
     }
   }
